5a5338d23645a1408f883cd71247d803f358f563
Clement.6 <tekamtenda@gmail.com>
2025-05-09 13:52:34 +0100

    bottoms tabs ajustement

--- app/screen/(tabs)/_layout.tsx
+++ app/screen/(tabs)/_layout.tsx
@@ -2,17 +2,39 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Stack, Tabs } from 'expo-router';
 import { tabsStyle } from '@/app/style/tabsStyle';
+import { FontAwesome6, Ionicons } from '@expo/vector-icons';
 
 export default function Layout () {
   return (
+    <Tabs
+      screenOptions={({route}) => ({
+        tabBarStyle: tabsStyle.tabsStyle,
+        tabBarShowLabel: false,
+        tabBarIconStyle: tabsStyle.iconStyle,
+        tabBarActiveTintColor: '#4b8bf8',
+        tabBarInactiveTintColor: '#4b8bf8',
+        tabBarIcon: ({ focused, color, size }) => {
+          type IconName = 'home' | 'home-outline' | 'person' | 'person-outline' | 'people' | 'people-outline' | 'settings' | 'settings-outline';
+          let iconName : IconName = 'home-outline';
+          if (route.name === 'home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          } else if (route.name === 'user') {
+            iconName = focused ? 'people' : 'people-outline';
+          } else if (route.name === 'setting') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          }
+          return <Ionicons name={iconName} size={size} color={color}/>;
+        }
 
-   <Tabs screenOptions={{tabBarStyle: tabsStyle.tabsStyle, tabBarShowLabel: false}}>
-    <Tabs.Screen name="home" options={{headerShown: false}} />
-    <Tabs.Screen name="profile" options={{headerShown: false}} />
-    <Tabs.Screen name="user" options={{headerShown: false}} />
-    <Tabs.Screen name="setting" options={{headerShown: false}} />
-   </Tabs>
-
+      })}
+    >
+      <Tabs.Screen name="home" options={{ headerShown: false }} />
+      <Tabs.Screen name="profile" options={{ headerShown: false }} />
+      <Tabs.Screen name="user" options={{ headerShown: false }} />
+      <Tabs.Screen name="setting" options={{ headerShown: false }} />
+    </Tabs>
   );
 
 }
--- app/style/tabsStyle.ts
+++ app/style/tabsStyle.ts
@@ -5,13 +5,18 @@ const height = Dimensions.get('screen').height;
 
 export const tabsStyle = StyleSheet.create({
     tabsStyle:{
-        borderRadius: 10,
+        borderRadius: 20,
         marginBottom: 10,
         marginHorizontal: 38,
         position: 'absolute',
         opacity: 0.9,
-        height: height*0.089,
-        paddingHorizontal: 20,
+        height: height*0.05,
+        width: screen*0.8
+        
+    },
+
+    iconStyle: {
+        marginTop: 4
     }
     
 })
